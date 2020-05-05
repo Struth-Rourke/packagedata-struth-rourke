3d94c9af0b3817de52532e3b0a54e3517af01a9b
Rourke Struthers <rourkestruthers@rourkes-mbp.lan>
2020-05-05 14:07:09 -0400

    More Polo properties

--- newpandaspackage/polos.py
+++ newpandaspackage/polos.py
@@ -10,9 +10,14 @@ class Polo():
         self.color = color
         pass
 
-    def wash(self): # Functions we define inside the Class for METHODS
-        print('WASHING')
+    @property # decorator - doesn't need parenthesis
+    def full_name(self:)
+    return f'{self.size} {self.color}'
 
+    def wash(self): # Functions we define inside the Class for METHODS
+        print(f'WASHING {self.size} {self.color} POLO!')
+
+    @staticmethod # Doesn't require self 
     def fold(self):
         print('FOLDING')
 
@@ -27,9 +32,11 @@ if __name__ == "__main__":
 
     p1 = Polo(size = 'Small', color = 'Blue')
     print(p1.size, p1.color)
+    print(p1.full_name)
     p1.wash()
 
 
     p2 = Polo(size = 'Large', color = 'Yellow')
     print(p2.size, p2.color)
+    print(p2.full_name)
     p2.fold()
