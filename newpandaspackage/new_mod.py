fc965c77ce7eabbb3729ecc62d8efc927c1c55a7
Rourke Struthers <rourkestruthers@rourkes-mbp.lan>
2020-05-05 15:41:58 -0400

    Changes to the package; adding additional functions

--- newpandaspackage/new_mod.py
+++ newpandaspackage/new_mod.py
@@ -2,44 +2,93 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
+# train_val_test Function
+def train_val_test_func(df):
+    ''' Takes in a dataframe and creates a training, validation, and test
+    df from the existing and returns the three types'''
 
-if __name__ == "__main__":
-    # Train , Val, Test Function
-    def train_val_test_func(df):
-        ''' Takes in a dataframe and creates a training, validation, and test
-        df from the existing and returns the three types'''
-        # Creating a copy
-        df = df.copy()
-            
-        # Creating a train/val/test split from df
-        ## train and test split
-        train, test = train_test_split(df, train_size = .9, test_size = .1, random_state = 42)
+    # Creating a copy
+    df = df.copy()
+        
+    # Creating a train/val/test split from df
+    ## train and test split
+    train, test = train_test_split(df, train_size = .9, test_size = .1, random_state = 42)
 
-        ## train and val split
-        train, val = train_test_split(train, train_size = .8, test_size = .2, random_state = 42)
+    ## train and val split
+    train, val = train_test_split(train, train_size = .8, test_size = .2, random_state = 42)
 
-        # Printing the shapes for each
-        print(train.shape, val.shape, test.shape)
+    # Printing the shapes for each
+    print(train.shape, val.shape, test.shape)
 
-        # Returning the df's
-        return train, val, test
+    # Returning the df's
+    return train, val, test
 
-    # Date Splitting Function
-    def date_splitting(Y):
-        ''' Takes a column with dates and creates three new columns with different
-        date types'''
 
-        # Creating a column copy
-        Y = Y.copy()
-            
-        # Inferring pandas datetime format
-        Y['Date Column'] = pd.to_datetime(Y['a'], infer_datetime_format = True)
+# date_splitting Function
+def date_splitting(Y):
+    ''' Takes a column with dates and creates three new columns with different
+    date types'''
 
-        # Creating new columns based on the date length (Y, M, D)
-        Y['Year'] = Y['Date Column'].dt.to_period('Y')
-        Y['Year_Month'] = Y['Date Column'].dt.to_period('M')
-        Y['Year_Month_Day'] = Y['Date Column'].dt.to_period('D')
+    # Creating a column copy
+    Y = Y.copy()
+        
+    # Inferring pandas datetime format
+    Y['Date Column'] = pd.to_datetime(Y['a'], infer_datetime_format = True)
 
-        return print(Y)
+    # Creating new columns based on the date length (Y, M, D)
+    Y['Year'] = Y['Date Column'].dt.to_period('Y')
+    Y['Year_Month'] = Y['Date Column'].dt.to_period('M')
+    Y['Year_Month_Day'] = Y['Date Column'].dt.to_period('D')
+
+    return print(Y)
+
+
+# add_state_names Function
+def add_state_names(my_df):
+    ''' Converts a dataframe with a column of state abbreviations,
+    adding a corresponding column of state names
+
+    Params:
+        my_df a pandas.DataFrame with a column called "abbrev".
+
+        Example:
+        add_state_names(DataFrame({'abbrev': ['CA', 'CO', 'CT', 'DC', 'TX']}))
+
+    Returns: a pandas.DataFrame with the original col as well as a name column
+    '''
+
+    # State Abbreviation -> Full name and Vice Versa
+    # FL -> Florida
+
+    # Creating a copy
+    new_frame = my_df.copy()
+
+    # need a list of dict with the abbrev/name mappings
+    names_map = {'CA': 'Cali', 'CO': 'Colo', 'CT': 'Conn', 'DC': 'District of Columbia'}
+        
+    # create a new column which maps the existing column using our names map
+    # breakpoint() ## Allows us to enter the script and adjust the things above it in the code
+    # type(type(new_frame['abbrev'])) -> Series
+    # Can use 'Dir(new_frame['abbrev'])' to see what functions/methods you can call on the particle datatype you're using
+
+    new_frame['name'] = new_frame['abbrev'].map(names_map)
+
+    return new_frame
+
+
+# list_series_df_col Function
+def list_series_df_col(lst, col_header):
+    ''' Takes a list, converts it to a series and then adds it as a column
+    inside a DataFrame.
+    '''
+
+    # taking in the list and converting it to a Series;
+    # assigning it to variable x
+    x = pd.Series(lst)
+
+    # Creating a DataFrame
+    df = pd.DataFrame(x, columns = col_header)
+
+    return df
 
 
--- newpandaspackage/pandas_package.py
+++ newpandaspackage/pandas_package.py
@@ -1,18 +1,42 @@
 import pandas as pd
-from newpandaspackage.new_mod import train_val_test_func, date_splitting
+from newpandaspackage.new_mod import train_val_test_func, date_splitting, add_state_names, list_series_df_col
 
-## Function 1
-# Mock df for train_val_test_func function
+## Function 1 (train_val_test_func)
+# Mock df
 dfx = pd.DataFrame({'a': [1,2,3,4,5,6,7,8,9,10], 'b': [1,2,3,4,5,6,7,8,9,10]})
 
-# Calling function on above df
+# Calling function
 train_val_test_func(dfx)
 
 
-## Function 2
-# Mock df for date_splitting function
-dfz = pd.DataFrame({'a': ['01/01/2019', '01/02/2019', '01/03/2019']})
 
-# Calling function on above df
-date_splitting(dfz)
+## Function 2 (date_splitting)
+# Mock df
+dfy = pd.DataFrame({'a': ['01/01/2019', '01/02/2019', '01/03/2019']})
 
+# Calling function
+date_splitting(dfy)
+
+
+
+## Function 3 (add_state_names)
+# Mock df
+dfz = DataFrame({'abbrev': ['CA', 'CO', 'CT', 'DC', 'TX']})
+print(df.head())
+
+# Calling and assigning function
+df2 = add_state_names(df)
+print(df2.head())
+
+
+## Function 4 (list_series_df_col)
+# Mock df
+dfq = [1, 2, 3, 4, 5]
+# Mock header
+header = 'Numbers'
+
+# Calling and printing the Function
+print(list_series_df_col(dfq, header))
+
+
+## Class / Methods / Functions
