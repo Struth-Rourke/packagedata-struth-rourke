1786bb2c9f9714b6acd08abef915835a48183523
Rourke Struthers <rourkestruthers@rourkes-mbp.lan>
2020-05-05 13:20:32 -0400

    Revised doc string and some Pep8 guidlines, plus code revisions

--- newpandaspackage/new_function.py
+++ newpandaspackage/new_function.py
@@ -2,28 +2,41 @@ from pandas import DataFrame
 
 # Defining the function
 def add_state_names(my_df):
-        # State Abbreviation -> Full name and Vice Versa
-        # FL -> Florida
+    ''' Converts a dataframe with a column of state abbreviations,
+    adding a corresponding column of state names
 
-        # Creating a copy 
-        new_frame = my_df.copy()
-
-        # need a list of dict with the abbrev/name mappings
-        names_map = {'CA': 'Cali', 'CO':'Colo', 'CT': 'Conn', 'DC': 'District of Columbia'}
+    Params: 
+        my_df a pandas.DataFrame with a column called "abbrev".
         
-        # create a new column which maps the existing column using our names map
-        # breakpoint() ## Allows us to enter the script and adjust the things above it in the code
-        # type(type(new_frame['abbrev'])) -> Series
-        # Can use 'Dir(new_frame['abbrev'])' to see what functions/methods you can call on the particle datatype you're using
+        Example: 
+        add_state_names(DataFrame({'abbrev': ['CA', 'CO', 'CT', 'DC', 'TX']}))
 
-        new_frame['name'] = new_frame['abbrev'].map(names_map)
+    Returns: a pandas.DataFrame with the original col as well as a name column
+    '''
 
-        return new_frame
+    # State Abbreviation -> Full name and Vice Versa
+    # FL -> Florida
+
+    # Creating a copy 
+    new_frame = my_df.copy()
+
+    # need a list of dict with the abbrev/name mappings
+    names_map = {'CA': 'Cali', 'CO': 'Colo', 'CT': 'Conn', 'DC': 'District of Columbia'}
+        
+    # create a new column which maps the existing column using our names map
+    # breakpoint() ## Allows us to enter the script and adjust the things above it in the code
+    # type(type(new_frame['abbrev'])) -> Series
+    # Can use 'Dir(new_frame['abbrev'])' to see what functions/methods you can call on the particle datatype you're using
+
+    new_frame['name'] = new_frame['abbrev'].map(names_map)
+
+    return new_frame
 
 
 if __name__ == "__main__":
+
     # Dataframe with State Abbrevs -- Type = Series
-    df = DataFrame({'abbrev':['CA','CO','CT','DC','TX']})
+    df = DataFrame({'abbrev': ['CA', 'CO', 'CT', 'DC', 'TX']})
     print(df.head())
 
     # Calling function and assigning it to a variable
