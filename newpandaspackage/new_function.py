7b998ddd17976811d7161dbc7c5b70ad92d17ac9
Rourke Struthers <rourkestruthers@rourkes-mbp.lan>
2020-05-06 13:24:09 -0400

    Refactored OOP again and changed functions for inheritance approach

--- newpandaspackage/new_function.py
+++ newpandaspackage/new_function.py
@@ -1,27 +1,22 @@
 from pandas import DataFrame
 
 
-class Wrangler(object):
+class WrangledFrame(DataFrame):
     '''
     Wrangler takes a dataframe and manipulates it
-    '''
-    def __init__(self, my_df):
-        '''
-        Params:
-            my_df a pandas.DataFrame with a column called "abbrev".
+    
+    Params:
+    my_df a pandas.DataFrame with a column called "abbrev".
             
-            Example:
-            add_state_names(DataFrame({'abbrev': ['CA', 'CO', 'CT', 'DC', 'TX']}))
-        
-        '''
-        self.df = my_df
+        Example:
+         add_state_names(DataFrame({'abbrev': ['CA', 'CO', 'CT', 'DC', 'TX']}))
+    '''
 
     # Defining the function
     def add_state_names(self):
         ''' Converts a dataframe with a column of state abbreviations,
         adding a corresponding column of state names
 
-        Returns: a pandas.DataFrame with the original col as well as a name column
         '''
 
         # State Abbreviation -> Full name and Vice Versa
@@ -35,12 +30,12 @@ class Wrangler(object):
         # type(type(new_frame['abbrev'])) -> Series
         # Can use 'Dir(new_frame['abbrev'])' to see what functions/methods you can call on the particle datatype you're using
 
-        self.df['name'] = self.df['abbrev'].map(names_map)
+        self['name'] = self['abbrev'].map(names_map)
 
 
     # Defining a function
     def inspect_columns(self):
-        print(self.df.columns)
+        print(self.columns)
 
 
 
@@ -57,12 +52,19 @@ if __name__ == "__main__":
     # print(df2.head())
 
 
-    df = DataFrame({'abbrev': ['CA', 'CO', 'CT', 'DC', 'TX']})
-    print(df.head())
+    # df = DataFrame({'abbrev': ['CA', 'CO', 'CT', 'DC', 'TX']})
+    # print(df.head())
 
     
-    wrangler = Wrangler(df)
-    print(type(wrangler))
-    wrangler.inspect_columns()
-    wrangler.add_state_names()
-    print(wrangler.df.head())
+    # wrangler = Wrangler(df)
+    # print(type(wrangler))
+    # wrangler.inspect_columns()
+    # wrangler.add_state_names()
+    # print(wrangler.df.head())
+
+
+    wf = WrangledFrame({'abbrev': ['CA', 'CO', 'CT', 'DC', 'TX']})
+    print(wf.head())
+    wf.add_state_names()
+    print(wf.head())
+    wf.inspect_columns()
